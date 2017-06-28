f6533c66310dbbbad831d6997cc321d293281207
Seth Dittebrand <sdittebrand@gmail.com>
2017-06-28 09:32:59 -0400

    Tells user they cannot divide by zero

--- division.rb
+++ division.rb
@@ -1,15 +1,20 @@
 def first()
     puts "Give me a number to divide"
-    f = gets
+    f = gets.to_i
 end
 
 def second()
     puts "Give me a number to divide it by"
-    s = gets
+    s = gets.to_i
 end
 
 f = first()
 s = second()
 
-puts "Answer : #{f.to_i} / #{s.to_i} = #{f.to_i / s.to_i}"
+if s == 0
+    puts "You can't divide by zero"
+    
+else puts "Answer : #{f.to_i} / #{s.to_i} = #{f.to_i / s.to_i}"
+end
+
 
